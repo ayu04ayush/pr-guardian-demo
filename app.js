30e9405514c80b44ecba7653929eec43f3832400
Ayush Mahto <ayushpersonal69@gmail.com>
2026-09-17 15:52:01 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+function getUser(user) {
+    return user.name;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+const password = "MySecretPassword123";
+
+console.log(getUser(null));
+console.log(divide(10, 0));
